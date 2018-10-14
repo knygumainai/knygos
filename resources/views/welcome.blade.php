b75d8748f81ab3f499e78c1f2c2edf403b983039
HP\MyPc <ramanauskaiteskaiste@gmail.com>
2018-10-14 17:58:42 +0300

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Knygų mainai
+                    Knygų mainai!
                 </div>
 
                 <div class="links">
